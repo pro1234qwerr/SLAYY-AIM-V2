f61927032089dc8c43c605eaf6de7d7cf71d9493
pro1234qwerr <159695740+pro1234qwerr@users.noreply.github.com>
2024-02-17 08:57:21 +0000

    Update SLAYY AIM.py

--- SLAYY AIM.py
+++ SLAYY AIM.py
@@ -7,6 +7,73 @@ import subprocess
 import re
 import urllib.request
 
+from keyauth import api
+import sys
+import time
+import platform
+import os
+import hashlib
+import colorama
+from time import sleep
+from colorama import Fore
+
+colorama.init()
+
+def clear():
+    if platform.system() == 'Windows':
+        os.system('cls & title Python Example')  # clear console, change title
+    elif platform.system() == 'Linux':
+        os.system('clear')  # clear console
+        sys.stdout.write("\x1b]0;Python Example\x07")  # change title
+    elif platform.system() == 'Darwin':
+        os.system("clear && printf '\e[3J'")  # clear console
+        os.system('''echo -n -e "\033]0;Python Example\007"''')  # change title
+
+print(Fore.LIGHTBLACK_EX + "Initializing")
+time.sleep(0.9)
+print(Fore.LIGHTBLACK_EX + "connecting")
+
+
+def getchecksum():
+    md5_hash = hashlib.md5()
+    file = open(''.join(sys.argv), "rb")
+    md5_hash.update(file.read())
+    digest = md5_hash.hexdigest()
+    return digest
+
+
+keyauthapp = api(
+    name = "SLAYY",
+    ownerid = "91id0HD1i4",
+    secret = "c391c003a2d235ece87fb4fcd14e3f39c67c5b8b80124dcd40d8b3bdfe5beeb5",
+    version = "1.0",
+    hash_to_check = getchecksum()
+)
+def answer():
+    try:
+        print(Fore.LIGHTMAGENTA_EX + """
+███████╗██╗      █████╗ ██╗   ██╗██╗   ██╗     █████╗ ██╗███╗   ███╗
+██╔════╝██║     ██╔══██╗╚██╗ ██╔╝╚██╗ ██╔╝    ██╔══██╗██║████╗ ████║
+███████╗██║     ███████║ ╚████╔╝  ╚████╔╝     ███████║██║██╔████╔██║
+╚════██║██║     ██╔══██║  ╚██╔╝    ╚██╔╝      ██╔══██║██║██║╚██╔╝██║
+███████║███████╗██║  ██║   ██║      ██║       ██║  ██║██║██║ ╚═╝ ██║
+╚══════╝╚══════╝╚═╝  ╚═╝   ╚═╝      ╚═╝       ╚═╝  ╚═╝╚═╝╚═╝     ╚═╝             
+""")
+
+        time.sleep(2)
+
+        print(Fore.LIGHTMAGENTA_EX + "[Slayy Aim] " + Fore.RESET + "Welcome to Slayy Aim")
+        time.sleep(0.5)
+
+        key = input("Enter your license key password: ")
+        keyauthapp.license(key)
+            
+    except KeyboardInterrupt:
+        os._exit(1)
+
+
+answer()
+
 try:
     newest_version = "https://raw.githubusercontent.com/pro1234qwerr/SLAYY-AIM-V2/main/current_version.txt"
     req = urllib.request.Request(newest_version, headers={'Cache-Control': 'no-cache'})
